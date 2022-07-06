423231b3c8eea2ca4de6a318267464e8f1a15ae3
boojack <stevenlgtm@gmail.com>
2022-07-06 15:17:13 +0800

    Revert "fix: using echo static middleware to embed frontend (#1766)"
    This reverts commit 68c46da4f6d550885105495db89da14af21c1a47.

--- server/server.go
+++ server/server.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"context"
+	"embed"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"net/http"
 	"runtime"
 	"strings"
@@ -80,15 +82,26 @@ var casbinDBAPolicy string
 //go:embed acl_casbin_policy_developer.csv
 var casbinDeveloperPolicy string
 
-// embedFrontend using the echo static middleware to embed the dist folder built from SPA.
-// refer: https://echo.labstack.com/middleware/static/
+//go:embed dist
+var embeddedFiles embed.FS
+
+func getFileSystem() http.FileSystem {
+	fs, err := fs.Sub(embeddedFiles, "dist")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fs)
+}
+
+// By default, we embed a placeholder index.html. If we want to build a monolithic binary including
+// both frontend and backend (e.g. to produce our release build), we will instruct the build process
+// to copy over the frontend artifacts and overwrite that placeholder.
 func embedFrontend(e *echo.Echo) {
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root:   "server/dist",
-		Index:  "index.html",
-		Browse: true,
-		HTML5:  true,
-	}))
+	// Catch-all route to return dist files, this is to prevent 404 when accessing non-root url.
+	// See https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually
+	frontendDistHandler := http.FileServer(getFileSystem())
+	e.GET("/*", echo.WrapHandler(frontendDistHandler))
 }
 
 // Use following cmd to generate swagger doc
